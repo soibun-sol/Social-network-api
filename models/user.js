3bce34c724d734e5bc2cb2e51d5daa655cb20a06
Keegan Hinton <khinton386@gmail.com>
2024-12-16 19:04:07 -0500

    started base line code for server. added in routes/api and stareted models

---
+++ models/user.js
@@ -0,0 +1,48 @@
+import { match } from 'assert';
+import e from 'express';
+import pkg from 'mongoose';
+const { Schema, model } = pkg;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+export default User;
